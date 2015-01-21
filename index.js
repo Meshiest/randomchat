01935ca0d2416f66252a7f899ca554b679b3c242
Meshiest <meek.mesh@gmail.com>
2015-01-21 11:29:08 -0600

    init

---
+++ index.js
@@ -0,0 +1,33 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+var idCount = 0;
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket) {
+
+  var id = idCount++;
+
+  socket.broadcast.emit('connection', id);
+
+  console.log('user '+id+' connected');
+
+  socket.on('chat message', function(msg) {
+    console.log('message('+id+'): ' + msg);
+    io.emit('chat message', id, msg);
+  });
+
+  socket.on('disconnect', function() {
+    socket.broadcast.emit('disconnection', id);
+    console.log('user '+id+' disconnected');
+  });
+});
+
+http.listen(3000, function() {
+  console.log('listening on *:3000');
+});
+
