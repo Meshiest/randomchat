b8d420926d69bb53521ab345403718f73f6efdd0
Meshiest <meek.mesh@gmail.com>
2015-01-21 11:52:10 -0600

    added names

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var fs = require('fs');
+var _ = require('underscore');
+
+_.mixin({
+  capitalize: function(string) {
+    return string.charAt(0).toUpperCase() + string.substring(1).toLowerCase();
+  }
+});
+
+var nounArr = [];
+fs.readFile(__dirname + '/noun.txt', 'utf-8', function(err, data) {
+  if(!err) {
+    nounArr = data.split("\n")
+    console.log("Found "+nounArr.length+" nouns")
+  }
+});
+
+var adjArr = [];
+fs.readFile(__dirname + '/adj.txt', 'utf-8', function(err, data) {
+  if(!err) {
+    adjArr = data.split("\n")
+    console.log("Found "+adjArr.length+" adjectives")
+  }
+});
+
+app.set('port', (process.env.PORT || 5000));
 
 var idCount = 0;
 
@@ -11,23 +37,24 @@ app.get('/', function(req, res) {
 io.on('connection', function(socket) {
 
   var id = idCount++;
+  var name = _(_.sample(adjArr)).capitalize()+" "+_(_.sample(nounArr)).capitalize();
 
-  socket.broadcast.emit('connection', id);
+  socket.broadcast.emit('connection', name);
 
-  console.log('user '+id+' connected');
+  console.log('user '+name+' connected');
 
   socket.on('chat message', function(msg) {
-    console.log('message('+id+'): ' + msg);
-    io.emit('chat message', id, msg);
+    console.log('message('+name+'): ' + msg);
+    io.emit('chat message', name, msg);
   });
 
   socket.on('disconnect', function() {
-    socket.broadcast.emit('disconnection', id);
-    console.log('user '+id+' disconnected');
+    socket.broadcast.emit('disconnection', name);
+    console.log('user '+name+' disconnected');
   });
 });
 
-http.listen(process.env.PORT || 3000, function() {
-  console.log('listening on *:'+(process.env.PORT || 3000));
+http.listen(app.get('port'), function() {
+  console.log('listening on *:'+app.get('port'));
 });
 
