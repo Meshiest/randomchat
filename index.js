1b8ca261a870030a543c90ba6bbe2795a88186f9
Meshiest <meek.mesh@gmail.com>
2015-01-21 19:27:24 -0600

    Spam protection

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var fs = require('fs');
 var _ = require('underscore');
+var escape = require("html-escape");
 
 var users = {}
 
@@ -10,19 +11,32 @@ var command = {}
 
 command.nick = function(user, args) {
   var name = _(_.sample(adjArr)).capitalize().trim()+" "+_(_.sample(nounArr)).capitalize().trim();
-  user.socket.broadcast.emit('message', '<strong>'+user.name+"</strong> is now known as <strong>"+name+"</strong>.");
+  user.socket.broadcast.emit('message', '<b>'+user.name+"</b> is now known as <b>"+name+"</b>.");
   console.log(user.name+" changed name to "+name);
-  user.socket.emit('message', "You are now known as <strong>"+name+"</strong>.");
+  user.socket.emit('message', "You are now known as <b>"+name+"</b>.");
   user.name = name;
 }
 
 command.list = function(user, args) {
   var list = [];
   for(var u in users) {
-    list.push(users[u].name+"("+users[u].id+")");
+    list.push(users[u].name+(users[u]==user ? "(You)" : ""));
   }
-  user.socket.emit('message',"<strong>Online Users</strong>: "+list.join(", "));
+  user.socket.emit('message',"<b>Online Users("+list.length+")</b>: "+list.join(", "));
 }
+
+command.color = function(user, args) {
+  user.color = _(Math.random()*7+8).hex()+_(Math.random()*7+8).hex()+_(Math.random()*7+8).hex();
+  user.socket.emit('message', "Your color is now <b style='position:relative;padding:3px;border-radius:6px;background:#"+user.color+";'>"+user.color+"</b>.");
+}
+
+command.help = function(user, args) {
+  list = [];
+  for(var c in command) {
+    list.push('/'+c);
+  }
+  user.socket.emit('message',"<b>Commands</b>: "+list.join(', '));
+};
 
 _.mixin({
   capitalize: function(string) {
@@ -68,7 +82,9 @@ io.on('connection', function(socket) {
 
   user.name = name;
   user.id = id;
-  user.color = _(Math.random()*10+5).hex()+_(Math.random()*10+5).hex()+_(Math.random()*10+5).hex();
+  user.lastMessage = '';
+  user.lastMessageTime = '';
+  user.color = 'fff'
   console.log("color "+user.color);
   user.socket = socket;
 
@@ -77,27 +93,48 @@ io.on('connection', function(socket) {
   socket.broadcast.emit('connection', user.name);
 
   console.log('user '+user.name+' connected');
-  socket.emit('message', "Connected as <strong>"+user.name+"</strong>");
+  socket.emit('message', "Connected as <b>"+user.name+"</b>");
 
   command['nick'](user);
+  command['color'](user);
   command['list'](user);
+  command['help'](user);
 
-  socket.on('chat message', function(msg) {
-    if(msg.charAt(0) == '/') {
-      args = msg.substring(1).split(' ')
-      if(command.hasOwnProperty(args[0])) {
-        command[args[0]](user,args.shift);
+  socket.on('chatmessage', function(msg) {
+
+    if(msg.charAt(0) == '/') { //handle commands
+      args = msg.substring(1).split(' ') //split commands into arguments
+      if(command.hasOwnProperty(args[0])) { //check if command exists
+        command[args[0]](user,args.shift); //execute command
       } else {
-        socket.emit('message', '"<strong>'+args[0]+'</strong>" is not a valid command');
+        socket.emit('message', '"<b>'+args[0]+'</b>" is not a valid command');
       }
       return;
     }
+    
+    msg = escape(msg);
+
     if(msg.length == 0 || msg.length > 160) {
-      socket.emit('message', "Your message is empty or too long");
+      socket.emit('message', "Your message is empty or too long!");
       return;
     }
+    
+    if(msg == user.lastMessage) {
+      socket.emit('message', "You're being too repetitive!");
+      return;
+    }
+
+    var time = new Date().getTime();
+    if(time < user.lastMessageTime + 300) {
+      socket.emit('message', "You're sending messages too fast!");
+      return;
+    }
+
+    user.lastMessage = msg;
+    user.lastMessageTime = time;
+
     console.log('message('+user.name+'): ' + msg);
-    io.emit('chat message', user.name, user.color, msg);
+    io.emit('chatmessage', user.name, user.color, msg);
   });
 
   socket.on('disconnect', function() {
