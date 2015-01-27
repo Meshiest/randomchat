b32daa97022f9adadd3bfeb8e27e8a7e082feea3
Meshiest <meek.mesh@gmail.com>
2015-01-26 22:27:35 -0600

    more blacklist?

--- index.js
+++ index.js
@@ -108,13 +108,13 @@ command.whois = function(user, args) {
   if(targetid == -2) {
     user.socket.emit('message', -1, "Found more than one user, please be more specific");
   } else if(targetid != -1) {
-    var addr = users[targetid].socket.handshake.address;
+    var addr = users[targetid].socket.request.connection.remoteAddress;
     var style = getStyle(users[targetid].color)
     user.socket.emit('message', -1, "WHOIS");
     user.socket.emit('message', -1, "User: <b"+style+">"+users[targetid].name+"</b>");
     user.socket.emit('message', -1, "ID: "+targetid);
     user.socket.emit('message', -1, "Color: <b"+style+">"+users[targetid].color+"</b>");
-    user.socket.emit('message', -1, "Address: "+addr.address);
+    user.socket.emit('message', -1, "Address: "+addr);
   } else {
     user.socket.emit('message', -1, "Could not find a user to whois");
   }
@@ -374,7 +374,7 @@ app.get('/', function(req, res) {
 
 io.on('connection', function(socket) {
 
-  var bot = socket.handshake.address.address.indexOf("10.") == 0 && false;
+  var bot = socket.request.connection.remoteAddress.indexOf("10.") == 0 && false;
 
   var user = {};
   var id = idCount++;
@@ -402,7 +402,7 @@ io.on('connection', function(socket) {
 
   socket.broadcast.to(user.room).emit('connection', user.name);
 
-  log('user '+user.id+' connected '+socket.handshake.address.address);
+  log('user '+user.id+' connected '+socket.request.connection.remoteAddress);
   socket.emit('message', -1, "Connected as <b>"+user.name+"</b>");
 
   if(!bot) {
