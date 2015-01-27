ae558a3a37735ab6e6637a5683bc51f8b8204a7e
Meshiest <meek.mesh@gmail.com>
2015-01-27 07:13:37 -0600

    more blacklist?

--- index.js
+++ index.js
@@ -108,13 +108,12 @@ command.whois = function(user, args) {
   if(targetid == -2) {
     user.socket.emit('message', -1, "Found more than one user, please be more specific");
   } else if(targetid != -1) {
-    var addr = users[targetid].socket.handshake.address;
     var style = getStyle(users[targetid].color)
     user.socket.emit('message', -1, "WHOIS");
     user.socket.emit('message', -1, "User: <b"+style+">"+users[targetid].name+"</b>");
     user.socket.emit('message', -1, "ID: "+targetid);
     user.socket.emit('message', -1, "Color: <b"+style+">"+users[targetid].color+"</b>");
-    user.socket.emit('message', -1, "Address: "+addr);
+    user.socket.emit('message', -1, "Address: "+users[targetid].addr);
     user.socket.emit('message', -1, "joinTime: "+users[targetid].joinTime);
     user.socket.emit('message', -1, "online: "+(new Date().getTime()-users[targetid].joinTime));
   } else {
@@ -376,6 +375,7 @@ app.get('/', function(req, res) {
 
 io.on('connection', function(socket) {
 
+  var addr = socket.manager.handshaken[socket.id].remoteAddress;
   var bot =  false;
 
   var user = {};
@@ -384,6 +384,7 @@ io.on('connection', function(socket) {
 
   user.name = name;
   user.isBot = bot;
+  user.addr = addr;
   user.isAdmin = false;
   user.id = id;
   user.history = [];
